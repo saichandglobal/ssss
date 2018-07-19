1cfe534649c545c645ab7ce82de8b04fb592dc8c
John Jarvis <jarv@gitlab.com>
2018-07-19 11:23:18 +0200

    Adds some browser scripts.

---
+++ onboarding/browser-scripts/canary.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         gitlab canary
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Makes the navbar purple when the canary cookie is set.
+// @author       jarv
+// @match        https://gitlab.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    function getCookie(name) {
+        var dc = document.cookie;
+        var prefix = name + "=";
+        var begin = dc.indexOf("; " + prefix);
+        if (begin == -1) {
+            begin = dc.indexOf(prefix);
+            if (begin != 0) return null;
+        }
+        else
+        {
+            begin += 2;
+            var end = document.cookie.indexOf(";", begin);
+            if (end == -1) {
+                end = dc.length;
+            }
+        }
+        return decodeURI(dc.substring(begin + prefix.length, end));
+    }
+
+
+    var canaryCookie = getCookie("gitlab_canary");
+
+    if (canaryCookie !== null) {
+        document.getElementsByClassName("navbar")[0].style.backgroundColor = "#6610f2";
+    }
+})();
---
+++ onboarding/browser-scripts/merge-requests.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Check all for merge requests
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Check the checkboxes for merge requests (Remove source branch, Squash commits) 
+// @author       jarv
+// @match        https://gitlab.com/*/merge_requests/new*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    checkThem([].slice.call(document.querySelectorAll('input[type="checkbox"]')));
+    function checkThem(nodes) {
+        nodes.forEach(function(n) { n.checked = true });
+    }
+})();
