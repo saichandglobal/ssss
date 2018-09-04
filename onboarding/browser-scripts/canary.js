0d106db33e9320a9ef0eb6b9541856e7cef5f191
John Jarvis <jarv@gitlab.com>
2018-09-04 07:59:32 +0000

    Better canary script.

--- onboarding/browser-scripts/canary.js
+++ onboarding/browser-scripts/canary.js
@@ -2,16 +2,26 @@
 // @name         gitlab canary
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Makes the navbar purple when the canary cookie is set.
+// @description  try to take over the world!
 // @author       jarv
 // @match        https://gitlab.com/*
 // @match        https://gprd.gitlab.com/*
 // @match        https://gstg.gitlab.com/*
 // @grant        none
 // ==/UserScript==
+/*
+javascript:void((function(d){
+document.cookie='gitlab_canary=' + (document.cookie.indexOf('gitlab_canary=true') >= 0 ?  'false' : 'true') + ';domain=.gitlab.com;path=/;expires=' + new Date(Date.now() + 31536000000).toUTCString(); location.reload();})(document));
+
+*/
 
 (function() {
-
+    function setCnyCookie(value) {
+        var date = new Date(Date.now() + 31536000000)
+        var expires = "; expires=" + date.toUTCString();
+        document.cookie = "gitlab_canary=" + value + expires + ";domain=.gitlab.com;path=/";
+        location.reload();
+    }
     function getCookie(name) {
         var dc = document.cookie;
         var prefix = name + "=";
@@ -31,10 +41,18 @@
         return decodeURI(dc.substring(begin + prefix.length, end));
     }
 
-
+    function updateCny() {
+        var canaryCookie = getCookie("gitlab_canary");
+        (canaryCookie === "true") ? setCnyCookie("false") : setCnyCookie("true");
+    }
     var canaryCookie = getCookie("gitlab_canary");
-
-    if (canaryCookie !== null) {
+    cnyButtonText = (canaryCookie === "true") ? "Canary Enabled" : "Canary Disabled";
+    if (canaryCookie === "true") {
         document.getElementsByClassName("navbar")[0].style.backgroundColor = "#6610f2";
     }
+    var zNode = document.createElement ('div');
+    zNode.innerHTML = '<button style="height:100%" id="myButton" type="button"><b>'
+                + cnyButtonText + '</b><br /><small>click to toggle</small></button>';
+    zNode.onclick = updateCny;
+    document.getElementsByClassName("header-content")[0].appendChild(zNode);
 })();
